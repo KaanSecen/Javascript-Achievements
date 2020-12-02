bcc71d8dec5d59c73598be2a7b34573c2a2e6fc9
Kaan Secen <24114@ma-web.nl>
2020-12-02 12:24:21 +0100

    Website revamp

---
+++ opdrachten.js
@@ -0,0 +1,44 @@
+function linkNaarTrelloJS(){
+    window.open("https://trello.com/b/xo5TJzFr/javascript-achievements","_self")
+}
+
+function linkNaarTrelloJSGM(){
+    window.open("https://trello.com/b/MYfZTti8/sd1a","_self")
+}
+
+//Opdracht Turtle//
+function LNTurtle_L_1_GIT(){
+    window.open("https://github.com/KaanSecen/Javascript-Achievements/tree/main/JS-Turtle","_self")
+}
+
+function LNTurtle_L_1_WEB(){
+    window.open("http://24114.hosts1.ma-cloud.nl/Javascript-Achievements/JS-Turtle/Demo.html","_self")
+}
+
+//Opdracht Knop //
+function LNDemoKnop_L_1_GIT(){
+    window.open("https://github.com/KaanSecen/JS-Extra/tree/master/ButtonKnop","_self")
+}
+
+function LNDemoKnop_L_1_WEB(){
+    window.open("http://24114.hosts1.ma-cloud.nl/JavaExtra/ButtonKnop/Datumknop.html","_self")
+}
+
+//Opdracht Checkerboard //
+function LNCheckerboard_L_2_GIT(){
+    window.open("https://github.com/KaanSecen/Javascript-Achievements/blob/main/JS_B2_L2_Checkerboard/index.html","_self")
+}
+
+function LNCheckerboard_L_2_WEB(){
+    window.open("http://24114.hosts1.ma-cloud.nl/Javascript-Achievements/JS_B2_L2_Checkerboard/index.html","_self")
+}
+
+//Opdracht Box blocks //
+
+function LNBoxBlocks_L_2_GIT(){
+    window.open("https://github.com/KaanSecen/Javascript-Achievements/blob/main/JS_B2_L2_BOXBLOCKS/index.html","_self")
+}
+
+function LNBoxBlocks_L_2_WEB(){
+    window.open("http://24114.hosts1.ma-cloud.nl/Javascript-Achievements/JS_B2_L2_BOXBLOCKS/index.html","_self")
+}
